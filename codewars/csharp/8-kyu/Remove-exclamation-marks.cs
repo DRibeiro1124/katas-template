9de7b288d61d2bb75eb0d59b0ed501ddfc561f76
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-03-01 14:18:36 -0500

    added other solutions from last kata

--- codewars/csharp/8-kyu/Remove-exclamation-marks.cs
+++ codewars/csharp/8-kyu/Remove-exclamation-marks.cs
@@ -9,4 +9,22 @@ public class Kata
     }
 }
 
+/* OTHER SOLUTIONS FROM USERS IN CODEWARS:
+
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s) => s.Replace("!", "");
+}
+
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s) => s.Replace("!", string.Empty);
+}
+
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s) => string.Join("", s.Split('!'));
+}
+
+*/
 
