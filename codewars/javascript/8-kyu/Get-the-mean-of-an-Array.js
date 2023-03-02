2b55551e56c80e42a2311094131f18f04a56a58e
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-03-01 21:33:47 -0500

    finished 'mean of an array' in JS

---
+++ codewars/javascript/8-kyu/Get-the-mean-of-an-Array.js
@@ -0,0 +1,21 @@
+/*
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/
+
+function getAverage(marks) {
+  // a place to store the sum, initialized to zero
+  let sum = 0;
+  // iterate over the marks
+  for (let i = 0; i < marks.length; i += 1) {
+    // add the current mark to the sum
+    const mark = marks[i];
+    sum += mark;
+  }
+
+  // return the sum divided by the number of marks
+  return Math.floor(sum / marks.length);
+}
